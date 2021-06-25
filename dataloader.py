090a344edfa7b7b96a84b8bb5db31a0ad7544866
DatNgo <dat821168@gmail.com>
2021-06-25 19:52:13 +0700

    Update Davidsion dataset

--- dataloader.py
+++ dataloader.py
@@ -6,23 +6,24 @@ import torch
 
 
 class BertweetDataset(Dataset):
-    def __init__(self, file_path, tokenizer, label_maps, batch_size=32, max_length=128):
-        self.file_path = file_path
+    def __init__(self,
+                 data_file_path,
+                 bert_tokenizer,
+                 label_map,
+                 text_idx,
+                 class_idx,
+                 delimiter='\t',
+                 batch_size=32,
+                 max_length=128):
+        self.file_path = data_file_path
+        self.text_idx = text_idx
+        self.class_idx = class_idx
+        self.delimiter = delimiter
         self.bz = batch_size
         self.max_len = max_length
-        self.label_maps = label_maps
+        self.label_map = label_map
         self.examples = []
-        self.create_examples(tokenizer)
-
-    @staticmethod
-    def read_tsv(file_path):
-        samples = []
-        with open(file_path, 'r') as f:
-            tsv_reader = csv.reader(f, delimiter='\t')
-            next(tsv_reader)
-            for row in tsv_reader:
-                samples.append((row[1].strip(), row[2].strip()))
-        return samples
+        self.create_examples(bert_tokenizer)
 
     def __len__(self):
         return len(self.examples)
@@ -35,8 +36,18 @@ class BertweetDataset(Dataset):
         labels_id = torch.tensor(example[3], dtype=torch.long)
         return token_ids, token_type_ids, attention_masks, labels_id
 
-    def create_examples(self, tokenizer):
-        samples = self.read_tsv(self.file_path)
+    @staticmethod
+    def read_tsv(file_path,  text_idx, class_idx, delimiter='\t'):
+        samples = []
+        with open(file_path, 'r') as f:
+            tsv_reader = csv.reader(f, delimiter=delimiter)
+            next(tsv_reader)
+            for row in tsv_reader:
+                samples.append((row[text_idx].strip(), row[class_idx].strip()))
+        return samples
+
+    def create_examples(self, bert_tokenizer):
+        samples = self.read_tsv(self.file_path, self.text_idx, self.class_idx, self.delimiter)
         sidx, eidx = 0, self.bz
         pbar = tqdm(total=len(samples), position=0)
         while sidx <= len(samples):
@@ -44,11 +55,11 @@ class BertweetDataset(Dataset):
             if len(batch_samples) == 0:
                 break
             texts, labels = list(zip(*batch_samples))
-            label_ids = [self.label_maps[label] for label in labels]
-            encoded_inputs = tokenizer(text=texts,
-                                       max_length=self.max_len,
-                                       padding='max_length',
-                                       truncation='longest_first')
+            label_ids = [self.label_map[label] for label in labels]
+            encoded_inputs = bert_tokenizer(text=texts,
+                                            max_length=self.max_len,
+                                            padding='max_length',
+                                            truncation='longest_first')
             encoded_inputs["label_ids"] = label_ids
             self.examples.extend(list(zip(*encoded_inputs.values())))
             sidx += self.bz
@@ -59,10 +70,16 @@ class BertweetDataset(Dataset):
 if __name__ == "__main__":
     from transformers import AutoTokenizer
     from torch.utils.data import DataLoader
-    file_path = "./dataset/HASOC/english_dataset.tsv"
+    file_path = "./dataset/Davidson2017/labeled_data.csv"
     tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base", normalization=True)
-    label_maps = {"NOT": 0, "HOF": 1}
-    bertdata = BertweetDataset(file_path, tokenizer, label_maps, batch_size=32, max_length=128)
+    label_maps = {"0": 1, "1": 1, "2": 0}
+    bertdata = BertweetDataset(file_path, tokenizer, label_maps,
+                               text_idx=6,
+                               class_idx=5,
+                               delimiter=',',
+                               batch_size=32,
+                               max_length=128)
     dataiter = DataLoader(bertdata, batch_size=32, shuffle=True)
-    for batch in dataiter:
+    pbar = tqdm(dataiter, total=len(dataiter), position=0)
+    for batch in pbar:
         continue
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dataloader import BertweetDataset
 from utils import caculate_score, get_total_time, plot_loss
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassification, AdamW
 from torch.utils.data import DataLoader, SubsetRandomSampler
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
 from tqdm import tqdm
 
 import logging
@@ -10,8 +10,10 @@ import torch
 import yaml
 import copy
 import time
+import argparse
 
-logging.basicConfig(format='%(message)s')
+
+logging.basicConfig(format='%(message)s', level=logging.INFO, filemode='a')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -32,6 +34,34 @@ def build_model(opts, model_config):
     ]
     optimizer = AdamW(optimizer_grouped_parameters, lr=opts["learning_rate"])
     return model, optimizer
+
+
+def train(opts, model, optimizer, data_iter):
+    model.train()
+    train_bar = tqdm(data_iter, total=len(data_iter), desc='\tTRAIN:', position=0, leave=True)
+    train_loss = 0
+    train_preds, train_golds = [], []
+    start_time = time.time()
+    for batch in train_bar:
+        input_ids, token_type_ids, att_masks, label_ids = batch
+        if opts["device"] == "cuda":
+            input_ids = input_ids.to(opts["device"])
+            token_type_ids = token_type_ids.to(opts["device"])
+            att_masks = att_masks.to(opts["device"])
+            label_ids = label_ids.to(opts["device"])
+        outputs = model(input_ids=input_ids,
+                        attention_mask=att_masks,
+                        token_type_ids=token_type_ids,
+                        labels=label_ids)
+        outputs.loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        train_loss += outputs.loss.item()
+        train_preds += [y.argmax().item() for y in outputs.logits]
+        train_golds += label_ids.tolist()
+    acc_score, f1_macro, f1_weighted = caculate_score(train_golds, train_preds)
+    train_loss = train_loss / len(data_iter)
+    return model, train_loss, (acc_score, f1_macro, f1_weighted), get_total_time(start_time)
 
 
 def eval(opts, model, data_iter):
@@ -56,35 +86,47 @@ def eval(opts, model, data_iter):
         eval_golds += label_ids.tolist()
     acc_score, f1_macro, f1_weighted = caculate_score(eval_golds, eval_preds)
     eval_loss = eval_loss / len(data_iter)
-    logger.info(f"\tEVAL  - Time: {get_total_time(start_time)}; AVG Loss: {eval_loss:.6f}; "
-                f"Accurancy: {acc_score:.4f}; F1_maro: {f1_macro:.4f}; F1_weighted: {f1_weighted:.4f}")
-    return eval_loss, (acc_score, f1_macro, f1_weighted)
+    return eval_loss, (acc_score, f1_macro, f1_weighted), get_total_time(start_time)
 
 
-def main():
-    with open("finetuning_config.yaml", 'r') as stream:
+def main(config_file):
+    with open(config_file, 'r') as stream:
         try:
-            opts = yaml.safe_load(stream)
+            opts = yaml.load(stream)
         except yaml.YAMLError as exc:
             print(exc)
     opts["device"] = 'cuda' if torch.cuda.is_available() and opts["use_gpu"] else 'cpu'
     model_config = AutoConfig.from_pretrained(opts["model_name"],
-                                              num_labels=len(opts["labels"]),
+                                              num_labels=len(opts["label_maps"]),
                                               finetuning_task=opts["task_name"])
     tokenizer = AutoTokenizer.from_pretrained(opts["model_name"],
                                               normalization=True)
-    label_maps = {label: idx for idx, label in enumerate(opts["labels"])}
+    label_maps = opts["label_maps"]
     logger.info("Load dataset")
     train_dataset = BertweetDataset(opts["train_file_path"], tokenizer, label_maps,
+                                    text_idx=opts["text_idx"],
+                                    class_idx=opts["class_idx"],
+                                    delimiter=opts["delimiter"],
                                     batch_size=opts["train_batch_size"],
                                     max_length=opts["max_length"])
-    test_dataset = BertweetDataset(opts["test_file_path"], tokenizer, label_maps,
-                                   batch_size=opts["test_batch_size"],
-                                   max_length=opts["max_length"])
-    test_iter = DataLoader(test_dataset, batch_size=opts["test_batch_size"], shuffle=True)
+    test_dataset = None
+    if opts["test_file_path"] is not None and opts["test_file_path"] != '':
+        test_dataset = BertweetDataset(opts["test_file_path"], tokenizer, label_maps,
+                                       text_idx=opts["text_idx"],
+                                       class_idx=opts["class_idx"],
+                                       delimiter=opts["delimiter"],
+                                       batch_size=opts["test_batch_size"],
+                                       max_length=opts["max_length"])
+        test_iter = DataLoader(test_dataset, batch_size=opts["test_batch_size"], shuffle=True)
 
+    if test_dataset is None:
+        data_idxs = list(range(train_dataset.__len__()))
+        data_idxs, test_idxs = train_test_split(data_idxs, test_size=opts["test_split"], shuffle=True)
+        test_iter = DataLoader(train_dataset, batch_size=opts["test_batch_size"],
+                               sampler=SubsetRandomSampler(test_idxs))
+    else:
+        data_idxs = list(range(train_dataset.__len__()))
     kf = KFold(n_splits=opts["kfold"], random_state=opts["random_state"], shuffle=True)
-    data_idxs = list(range(train_dataset.__len__()))
     avg_scores = {"acc": [], "f1_macro": [], "f1_weighted": []}
     for idx, (train_idx, eval_idx) in enumerate(kf.split(data_idxs)):
         model, optimizer = build_model(opts, model_config)
@@ -97,38 +139,17 @@ def main():
         eval_iter = DataLoader(train_dataset,
                                batch_size=opts["test_batch_size"],
                                sampler=SubsetRandomSampler(eval_idx))
-        history = {"train_loss": [], "test_loss": []}
+        history = {"train_loss": [], "eval_loss": []}
         for epoch in range(opts["num_epochs"]):
             logger.info(f"Fold: {idx} - Epoch: {epoch+1}/{opts['num_epochs']}")
-            model.train()
-            train_bar = tqdm(train_iter, total=len(train_iter), desc='\tTRAIN:', position=0, leave=True)
-            train_loss = 0
-            train_preds, train_golds = [], []
-            start_time = time.time()
-            for batch in train_bar:
-                input_ids, token_type_ids, att_masks, label_ids = batch
-                if opts["device"] == "cuda":
-                    input_ids = input_ids.to(opts["device"])
-                    token_type_ids = token_type_ids.to(opts["device"])
-                    att_masks = att_masks.to(opts["device"])
-                    label_ids = label_ids.to(opts["device"])
-                outputs = model(input_ids=input_ids,
-                                attention_mask=att_masks,
-                                token_type_ids=token_type_ids,
-                                labels=label_ids)
-                outputs.loss.backward()
-                optimizer.step()
-                optimizer.zero_grad()
-                train_loss += outputs.loss.item()
-                train_preds += [y.argmax().item() for y in outputs.logits]
-                train_golds += label_ids.tolist()
-            acc_score, f1_macro, f1_weighted = caculate_score(train_golds, train_preds)
-            train_loss = train_loss / len(train_iter)
-            logger.info(f'\tTRAIN  - Time: {get_total_time(start_time)}; AVG Loss: {train_loss:.6f}; '
-                        f'Accurancy: {acc_score:.4f}; F1_maro: {f1_macro:.4f}; F1_weighted: {f1_weighted:.4f}')
-            eval_loss, eval_score = eval(opts, model, eval_iter)
+            model, train_loss, train_score, train_time = train(opts, model, optimizer, train_iter)
+            logger.info(f'\tTRAIN  - Time: {train_time}; AVG Loss: {train_loss:.6f}; Accurancy: {train_score[0]:.4f}; '
+                        f'F1_maro: {train_score[1]:.4f}; F1_weighted: {train_score[2]:.4f}')
+            eval_loss, eval_score, eval_time = eval(opts, model, eval_iter)
+            logger.info(f"\tEVAL  - Time: {eval_time}; AVG Loss: {eval_loss:.6f}; Accurancy: {eval_score[0]:.4f}; "
+                        f"F1_maro: {eval_score[1]:.4f}; F1_weighted: {eval_score[2]:.4f}")
             history["train_loss"].append(train_loss)
-            history["test_loss"].append(eval_loss)
+            history["eval_loss"].append(eval_loss)
             if best_score <= eval_score[1]:
                 best_model = copy.deepcopy(model)
                 best_score = eval_score[1]
@@ -141,12 +162,16 @@ def main():
                 counter += 1
             if counter >= opts["early_stop"]:
                 break
+
         logger.info(f"Test at epoch {best_epoch+1}:")
-        _, scores = eval(opts, best_model, test_iter)
-        avg_scores["acc"].append(scores[0])
-        avg_scores["f1_macro"].append(scores[1])
-        avg_scores["f1_weighted"].append(scores[2])
-        plot_loss(history["train_loss"], history["test_loss"], opts["output_path"])
+        test_loss, test_score, test_time = eval(opts, best_model, test_iter)
+        logger.info(f"\tTEST  - Time: {test_time}; AVG Loss: {test_loss:.6f}; Accurancy: {test_score[0]:.4f}; "
+                    f"F1_maro: {test_score[1]:.4f}; F1_weighted: {test_score[2]:.4f}")
+        avg_scores["acc"].append(test_score[0])
+        avg_scores["f1_macro"].append(test_score[1])
+        avg_scores["f1_weighted"].append(test_score[2])
+        plot_loss(history["train_loss"], history["eval_loss"], opts["output_path"])
+
     logger.info("Summary:")
     logger.info(f"\tAccurancy: {(sum(avg_scores['acc'])/opts['kfold']):.4f}")
     logger.info(f"\tF1 Macro: {(sum(avg_scores['f1_macro'])/opts['kfold']):.4f}")
@@ -154,4 +179,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", default=None, type=str, required=True,
+                        help="Config file path.")
+    args = parser.parse_args()
+    main(args.config_file)
